35c5913fc3d5ceac2003bb7f6c8678e68ebda61b
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-03-03 15:59:11 +0800

    first commit

---
+++ XPLClass/XPLClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XPLClass
+//
+//  Created by 朋朋 on 2020/3/3.
+//  Copyright © 2020 Upfloor5Go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
